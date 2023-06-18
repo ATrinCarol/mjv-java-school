c76c12d472691ba97ecd3e52d34510c7478d0535
Carol Trindade <caroline.alvestrindade@hotmail.com>
2023-06-18 13:58:06 -0300

    feat: primeira tentativa = gerar o arquivo: ok. gravar os dados: em evolução

---
+++ AtividadeArquivoCSV/src/PessoaCandidata.java
@@ -0,0 +1,87 @@
+import java.time.LocalDate;
+
+public class PessoaCandidata {
+    private String nome;
+    private String sobrenome;
+    private LocalDate dataNascimento;
+    private String sexo;
+    private String email;
+    private String telefone;
+    private String experienciaProfissional;
+    private Double faixaSalarial;
+    private Endereco endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getExperienciaProfissional() {
+        return experienciaProfissional;
+    }
+
+    public void setExperienciaProfissional(String experienciaProfissional) {
+        this.experienciaProfissional = experienciaProfissional;
+    }
+
+    public Double getFaixaSalarial() {
+        return faixaSalarial;
+    }
+
+    public void setFaixaSalarial(Double faixaSalarial) {
+        this.faixaSalarial = faixaSalarial;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+
+    }
+}
+
